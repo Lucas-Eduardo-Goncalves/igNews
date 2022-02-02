2f599105d2ce69696a6b19a7e1eac9c568e84e4d
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2022-02-02 18:40:47 -0300

    Adicionando requisiçao ao stripe e quase finalizando a tela home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './styles/home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,8 +26,10 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
@@ -24,3 +37,32 @@ export default function Home() {
     </>
   )
 };  
+
+// Criando função server side rendering 
+//  (Só pode ser criada em paginas, não em componentes)
+//  (O nome da função deve ser sempre o mesmo)
+//  (A função deve ser sempre ascincrona "async")
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  // Faço a chamada a api do stripe passando o id do preço
+  const price = await stripe.prices.retrieve('price_1KOo3RCWaPdJwNCYi7jLoJbs', {
+    // Utilizo esse "expand: ['product']" para ter acesso a todas as informações do produto
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+  // A pagina tem acesso as props retornadas aqui
+  return {
+    props: {
+      product
+    }
+  }
+} 
