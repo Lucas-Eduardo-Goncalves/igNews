bb794170d995fab7ff419a66bd3803d0e759f30c
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2022-02-03 08:42:14 -0300

    Finalizado login com github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLogged = true;
+  const { status, data } = useSession();
 
-  if(isUserLogged) {
+  if(status === "authenticated") {
     return (
       <button 
         type="button"
         className={styles.SignInButtonContainer}
+        onClick={() => signOut()}
       >
         <FaGithub color="#84D361"/>
-        Lucas Gonçalves
+        {data.user.name}
         <FiX color="#737380"/>
       </button>
     ) 
@@ -22,6 +25,7 @@ export function SignInButton() {
       <button 
         type="button"
         className={styles.SignInButtonContainer}
+        onClick={() => signIn('github')}
       >
         <FaGithub color="#EBA417"/>
         Sign in with Github
