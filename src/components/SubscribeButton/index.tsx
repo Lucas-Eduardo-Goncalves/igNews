3b16752f09aec5cc0b1e78e95261b8cbb8cad860
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2022-02-07 09:04:32 -0300

    Finalizado a criaçao de usuario no stripe e no fauna para compra de produto

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,9 +9,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { status } = useSession();
+
+  async function handleSubscribe() {
+    if(status !== 'authenticated') {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({
+        sessionId: sessionId
+      });
+
+    } catch(err) {
+      alert(err);
+    }
+  }
+
   return (
     <button
       type="button"
+      onClick={handleSubscribe}
       className={styles.subscribeButtonContainer}
     >
       Subscribe now
